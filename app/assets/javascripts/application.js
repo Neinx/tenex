6f00673c7b08e9caa94681542c7a5b9135a7b2c8
Neinx <shayansbn@yahoo.com>
2015-08-25 00:09:47 -0600

    first commit

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,44 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require jquery
+//= require jquery_ujs
+//= require bootstrap.js
+//= require backstretch.min.js
+//= require backstretch.min.js
+//= require contact.js
+//= require counter.js
+//= require custom.js
+//= require custom_countdown.js
+//= require custom_countto.js
+//= require custom_google-map.js
+//= require custom_hero.js
+//= require custom_portfolio.js
+//= require custom_skills.js
+//= require custom_ui-nav.js
+//= require imagesloaded.pkgd.js
+//= require imagesloaded.pkgd.min.js
+//= require inview.js
+//= require inview.min.js
+//= require isotope.pkgd.js
+//= require isotope.pkgd.min.js
+//= require jquery.countdown.js
+//= require jquery.countdown.min.js
+//= require jquery.plugin.js
+//= require jquery.plugin.min.js
+//= require jquery.waypoints.js
+//= require jquery.waypoints.min.js
+//= require npm.js
+//= require scrolltopcontrol.js
+//= require smoothscroll.js
+//= require wow.js
+//= require wow.min.js
